222614507f42b1df271ecede415b1d4ea7e3b68d
Alejandro Lavado <alejandro.lvd@gmail.com>
2019-12-08 13:46:49 -0300

    agrega estilos a pagina principal

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -9,13 +9,15 @@ import SeleccionEjercicio from '../SeleccionEjercicio';
 
 function App() {
   return (
-    <Switch>
-      <Route exact path="/" component={Principal} />
-      <Route exact path="/dass21" component={Dass21} />
-      <Route exact path="/subjetiva" component={MedicionSubjetiva} />
-      <Route exact path="/ejercicios" component={SeleccionEjercicio} />
-      <Route exact path="/otras" component={Otras} />
-    </Switch>
+    <div id="contenedor-app">
+      <Switch>
+        <Route exact path="/" component={Principal} />
+        <Route exact path="/dass21" component={Dass21} />
+        <Route exact path="/subjetiva" component={MedicionSubjetiva} />
+        <Route exact path="/ejercicios" component={SeleccionEjercicio} />
+        <Route exact path="/otras" component={Otras} />
+      </Switch>
+    </div>
   );
 }
 
--- src/components/Principal/Principal.js
+++ src/components/Principal/Principal.js
@@ -1,14 +1,18 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { useHistory } from 'react-router-dom'
 import './Principal.css'
 
 const Principal = () => {
+
+  const history = useHistory()
+  const navegar = path => () => history.push(path)
+
   return (
-    <div>
+    <div id="contenedor-principal">
       <div>ACHS MINDFULNESS</div>
-      <button><Link to="/dass21">dasd</Link></button>
-      <button><Link to="/subjetiva">medición subjetiva</Link></button>
-      <button><Link to="/ejercicios">ejercicio</Link></button>
+      <button onClick={navegar('/dass21')}>Responder DASS-21</button>
+      <button onClick={navegar('/subjetiva')}>Indicar estado de ánimo</button>
+      <button onClick={navegar('/ejercicios')}>Realizar ejercicio</button>
     </div>
   )
 }
