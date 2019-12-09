459262e63fd80a0b22ba8794ad1b5329b78aeaf9
Alejandro Lavado <alejandro.lvd@gmail.com>
2019-12-08 21:00:59 -0300

    agrega los 12 estados de animi

--- src/components/MedicionSubjetiva/MedicionSubjetiva.js
+++ src/components/MedicionSubjetiva/MedicionSubjetiva.js
@@ -1,21 +1,32 @@
 import React, { useState } from 'react'
 import './MedicionSubjetiva.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faDizzy, faTired, faFrown, faMeh, faSmile, faGrin, faLaughBeam } from '@fortawesome/free-solid-svg-icons'
+import { faLaughBeam, faSmileBeam, faSmile, faMeh, faSadTear, faFrown, faAngry, faFlushed, faLaughSquint, faFrownOpen, faTired } from '@fortawesome/free-solid-svg-icons'
 
 const MedicionSubjetiva = () => {
 
-  const [animoSeleccionado, setAnimoSeleccionado] = useState(0)
-
   const iconos = [
-    faDizzy, faTired, faFrown, faMeh, faSmile, faGrin, faLaughBeam
-  ]
+    faLaughSquint, // I. Pleasant activation (energetic, excited)
+    faLaughBeam, // II. Activated pleasure (enthusiastic, elated)
+    faSmileBeam, // III. Pleasure (satisfied, pleased)
+    faSmile, // IV. Deactivated pleasure (serene, peaceful)
+    faSmile, // V. Pleasant deactivation (placid, tranquil)
+    faMeh, // VI. Deactivation (quiet, still)
+    faFrownOpen, // VII. Unpleasant deactivation (sluggish, tired)
+    faSadTear, // VIII. Deactivated displeasure (sad, gloomy)
+    faFrown, // IX. Displeasure (unhappy, dissatisfied)
+    faAngry, // X. Activated displeasure (distressed, upset)
+    faTired, // XI. Unpleasant activation (frenzied, jittery)
+    faFlushed // XII. Activation (aroused, activated)
+  ].reverse()
+
+  const [animoSeleccionado, setAnimoSeleccionado] = useState(iconos.length / 2)
 
   return (
     <div id="contenedor-medicion-subjetiva">
       <h1>¿Cómo te sientes<br />en este momento?</h1>
       <div id="contenedor-icono-estado-animo">
-        <FontAwesomeIcon icon={iconos[animoSeleccionado]} size={"8x"} />
+        <FontAwesomeIcon icon={iconos[animoSeleccionado]} size={"6x"} />
       </div>
       <input
         id="slider-subjetivo"
