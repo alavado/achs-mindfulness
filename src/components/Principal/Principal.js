a51d8da534ebe04c35135fa672db2e64b28f37e2
Alejandro Lavado <alejandro.lvd@gmail.com>
2019-12-24 10:36:55 -0300

    cambia icono dass21

--- src/components/Principal/Principal.js
+++ src/components/Principal/Principal.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import './Principal.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faClipboardList as iconoDass21 } from '@fortawesome/free-solid-svg-icons'
 
 const Principal = () => {
 
   return (
     <div id="contenedor-principal">
-      <Link to="/dass21/0" id="link-dass21"><button>DASS21</button></Link>
+      <Link to="/dass21/0" id="link-dass21"><FontAwesomeIcon icon={iconoDass21} size="2x" /></Link>
       <h1>Mindfulness</h1>
       <div className="contenido-centrado">
         <Link to="/ejercicios">
