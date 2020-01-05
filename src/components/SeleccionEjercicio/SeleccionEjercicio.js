ff3a16f389995808c884d1014a656c93eb41b0f9
Alejandro Lavado <alejandro.lvd@gmail.com>
2020-01-04 23:49:23 -0300

    refactor meditaciones

--- src/components/SeleccionEjercicio/SeleccionEjercicio.js
+++ src/components/SeleccionEjercicio/SeleccionEjercicio.js
@@ -3,29 +3,33 @@ import audio1 from '../../assets/audio/audio1.ogg'
 import audio2 from '../../assets/audio/audio2.ogg'
 import './SeleccionEjercicio.css'
 
+const meditaciones = [
+  {
+    titulo: 'Meditación Natural',
+    descripcion: 'Una meditacion buena'
+  },
+  {
+    titulo: 'Meditación Corporal',
+    descripcion: 'Una meditacion de cuerpo entero'
+  }
+]
+
 const SeleccionEjercicio = () => {
   return (
     <div className="contenedor">
       <div className="contenido-centrado">
         <h1>Selecciona la meditación</h1>
-        <div className="contenedor-ejercicio">
-          <h1>Título</h1>
-          <p>Descripción</p>
-          <audio id="audio1">
-            <source src={audio1} type="audio/ogg" />
-          </audio>
-          <button onClick={() => document.getElementById("audio1").play()}>Play</button>
-          <button onClick={() => document.getElementById("audio1").pause()}>Pausa</button>
-        </div>
-        <div className="contenedor-ejercicio">
-          <h1>Título</h1>
-          <p>Descripción</p>
-          <audio id="audio2">
-            <source src={audio2} type="audio/ogg" />
-          </audio>
-          <button onClick={() => document.getElementById("audio2").play()}>Play</button>
-          <button onClick={() => document.getElementById("audio2").pause()}>Pausa</button>
-        </div>
+        {meditaciones.map(({ titulo, descripcion }) => (
+          <div className="contenedor-ejercicio">
+            <h1>{titulo}</h1>
+            <p>{descripcion}</p>
+            <audio id="audio1">
+              <source src={audio1} type="audio/ogg" />
+            </audio>
+            <button onClick={() => document.getElementById("audio1").play()}>Play</button>
+            <button onClick={() => document.getElementById("audio1").pause()}>Pausa</button>
+          </div>
+        ))}
       </div>
     </div>
   )
